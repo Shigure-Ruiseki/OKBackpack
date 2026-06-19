f2d69e7eee3c802ebfb6e1f09b964392808ea6d9
Shigure-Ruiseki <pn163163@gmail.com>
2026-06-20 01:17:55 +0700

    sa

--- src/main/java/ruiseki/okbackpack/compat/findit/BackpackProvider.java
+++ src/main/java/ruiseki/okbackpack/compat/findit/BackpackProvider.java
@@ -4,20 +4,27 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.fluids.FluidStack;
 
 import com.gtnh.findit.IStackFilter;
+import com.gtnh.findit.IStackFilter.AnyMultiItemFilter;
+import com.gtnh.findit.IStackFilter.FluidStackFilter;
 import com.gtnh.findit.IStackFilter.IStackFilterProvider;
 import com.gtnh.findit.IStackFilter.InventoryStackFilter;
 
+import ruiseki.okbackpack.api.wrapper.ITankUpgrade;
 import ruiseki.okbackpack.common.block.BackpackWrapper;
 import ruiseki.okbackpack.common.block.BlockBackpack;
+import ruiseki.okbackpack.common.block.TEBackpack;
 import ruiseki.okbackpack.common.helpers.BackpackEntityHelpers;
 
 public class BackpackProvider implements IStackFilterProvider {
 
     @Override
     public IStackFilter getFilter(EntityPlayer player, TileEntity tileEntity) {
-        return null;
+        if (!(tileEntity instanceof TEBackpack backpack)) return null;
+        if (backpack.getWrapper() == null) return null;
+        return getFilter(player, backpack.getWrapper());
     }
 
     @Override
@@ -28,10 +35,32 @@ public class BackpackProvider implements IStackFilterProvider {
             BackpackWrapper wrapper = BackpackEntityHelpers.getInteractionWrapper(player, stack);
 
             if (wrapper != null) {
-                return new InventoryStackFilter(player, wrapper);
+                return getFilter(player, wrapper);
             }
         }
 
         return null;
+    }
+
+    public IStackFilter getFilter(EntityPlayer player, BackpackWrapper wrapper) {
+        final AnyMultiItemFilter filter = new AnyMultiItemFilter();
+        final FluidStackFilter fluidFilter = new FluidStackFilter();
+
+        filter.add(new InventoryStackFilter(player, wrapper));
+        for (ITankUpgrade tank : wrapper.gatherCapabilityUpgrades(ITankUpgrade.class)
+            .values()) {
+            if (tank != null) {
+                FluidStack fluid = tank.getContents();
+                if (fluid != null && fluid.amount > 0) {
+                    fluidFilter.add(fluid);
+                }
+            }
+        }
+
+        if (!fluidFilter.isEmpty()) {
+            filter.add(fluidFilter);
+        }
+
+        return filter;
     }
 }
