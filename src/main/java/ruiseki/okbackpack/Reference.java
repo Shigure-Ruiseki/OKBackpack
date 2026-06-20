5c554f4ef809eadaf917b662b2b22446d3ee900a
Ruiseki <92140543+Shigure-Ruiseki@users.noreply.github.com>
2026-06-20 07:55:25 +0000

    ✨ feat(recipes): Add data-driven recipe system and numerous new upgrade recipes (#120)

--- src/main/java/ruiseki/okbackpack/Reference.java
+++ src/main/java/ruiseki/okbackpack/Reference.java
@@ -7,7 +7,7 @@ public class Reference {
     public static final String VERSION = Tags.VERSION;
     public static final String DEPENDENCIES = "required-after:gtnhlib@[0.11.9,);"
         + "required-after:modularui2@[2.3.73-1.7.10,);"
-        + "required-after:okcore@[26.06.19.1,);"
+        + "required-after:okcore@[26.06.20.0,);"
         + "after:Baubles;"
         + "after:TConstruct;"
         + "after:angelica;"
