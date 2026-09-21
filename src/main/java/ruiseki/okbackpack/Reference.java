1e0c60ac0206dd01e0db7533e43a6f1b2d64b154
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-09-21 13:37:08 +0700

    update readme

--- src/main/java/ruiseki/okbackpack/Reference.java
+++ src/main/java/ruiseki/okbackpack/Reference.java
@@ -5,9 +5,9 @@ public class Reference {
     public static final String MOD_ID = Tags.MOD_ID;
     public static final String MOD_NAME = Tags.MOD_NAME;
     public static final String MOD_VERSION = Tags.VERSION;
-    public static final String MOD_DEPENDENCIES = "required-after:gtnhlib@[0.11.45,);"
-        + "required-after:modularui2@[2.3.88-1.7.10,);"
-        + "required-after:okcore;"
+    public static final String MOD_DEPENDENCIES = "required-after:gtnhlib@[0.11.48,);"
+        + "required-after:modularui2@[2.3.90-1.7.10,);"
+        + "required-after:okcore@[26.09.21.0,);"
         + "after:Baubles;"
         + "after:TConstruct;"
         + "after:angelica;"
