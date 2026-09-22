98f63990b37fef088cc8e13e5aa6553b0e62dfde
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2026-09-22 12:25:36 +0800

    Update BackpackRecipeLoader.java

--- src/main/java/ruiseki/okbackpack/common/recipe/BackpackRecipeLoader.java
+++ src/main/java/ruiseki/okbackpack/common/recipe/BackpackRecipeLoader.java
@@ -2,23 +2,14 @@ package ruiseki.okbackpack.common.recipe;
 
 import java.util.Collection;
 
-import net.minecraft.util.ResourceLocation;
-
-import com.google.gson.JsonObject;
-
 import ruiseki.okbackpack.OKBackpack;
-import ruiseki.okbackpack.Reference;
 import ruiseki.okcore.recipe.IRecipeOK;
 import ruiseki.okcore.recipe.RecipeManager;
 import ruiseki.okcore.recipe.RecipeRegistry;
 
+// TODO: requires OKCore to fix asynchronous loading and server side recipe registration
+@Deprecated
 public class BackpackRecipeLoader {
-
-    public static final ResourceLocation SHAPED_TYPE_ID = new ResourceLocation("minecraft", "crafting_shaped");
-
-    public static final ResourceLocation SHAPELESS_TYPE_ID = new ResourceLocation("minecraft", "crafting_shapeless");
-
-    protected BackpackRecipeLoader() {}
 
     public static int publishRecipes() {
         RecipeManager manager = RecipeManager.getManager();
@@ -39,23 +30,5 @@ public class BackpackRecipeLoader {
         }
 
         return count;
-    }
-
-    public static IRecipeOK<?> parse(ResourceLocation id, JsonObject json) {
-        if (json == null || !json.has("type")) return null;
-
-        return RecipeRegistry.deserialize(id, json);
-    }
-
-    public static String getRecipeDirectory() {
-        return Reference.MOD_ID + "/recipes";
-    }
-
-    public static boolean isTypeRegistered(ResourceLocation id) {
-        return RecipeRegistry.getType(id) != null;
-    }
-
-    public static boolean isSerializerRegistered(ResourceLocation id) {
-        return RecipeRegistry.getSerializer(id) != null;
     }
 }
